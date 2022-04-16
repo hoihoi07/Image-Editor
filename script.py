ac604c7610ec730824aeda957a9a3efb8b24093a
hoihoi07 <97010331+hoihoi07@users.noreply.github.com>
2022-04-16 19:58:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -21,4 +21,4 @@ For more click help....</b>"""
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://t.me/jetbots'>Click Here</a>"""
